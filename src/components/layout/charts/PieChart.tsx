93c442275210c0f7868f158c458bcab219f4d1c7
Ryan <ryanz35@hotmail.com>
2023-02-24 14:34:24 -0500

    displaying header and value for dash header

--- src/components/layout/charts/PieChart.tsx
+++ src/components/layout/charts/PieChart.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from "react";
+import ReactApexChart from "react-apexcharts";
 import { Typography, Box, Stack } from "@pankod/refine-mui";
-import { PieChartProps } from 'interfaces/home';
+import { PieChartProps } from "interfaces/home";
 
-const PieChart = ({title, value, series, colors} : PieChartProps) => {
+const PieChart = ({ title, value, series, colors }: PieChartProps) => {
   return (
     <Box>
       <Stack direction="column">
-
+        <Typography fontSize={14}>{title}</Typography>
+        <Typography fontSize={24} color="#11142d" fontWeight={700} mt={1}>
+          {value}
+        </Typography>
+        
       </Stack>
+      
     </Box>
-  )
-}
+  );
+};
 
-export default PieChart
+export default PieChart;
