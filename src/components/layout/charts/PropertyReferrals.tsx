c936dcf9938902647e28ed5634f20ef17c195bbe
Ryan <ryanz35@hotmail.com>
2023-03-01 16:56:56 -0500

    title and percentage for bar

--- src/components/layout/charts/PropertyReferrals.tsx
+++ src/components/layout/charts/PropertyReferrals.tsx
@@ -1,6 +1,25 @@
 import { Box, Stack, Typography } from "@pankod/refine-mui";
 import { propertyReferralsInfo } from "constants/index";
 
+interface ProgressBarProps {
+  title: string,
+  percentage: number,
+  color: string,
+}
+
+const ProgressBar = ({ title, percentage, color }: ProgressBarProps) => (
+  <Box width="100%">
+    <Stack direction="row" alignItems="center" justifyContent="space-between">
+      <Typography fontSize={15} fontWeight={100} color="#11142d">
+        {title}
+      </Typography>
+      <Typography fontSize={15} fontWeight={100} color="#11142d">
+        {percentage}%
+      </Typography>
+    </Stack>
+  </Box>
+)
+
 const PropertyReferrals = () => {
   return (
     <Box
@@ -17,7 +36,9 @@ const PropertyReferrals = () => {
           Property Referrals
         </Typography>
         <Stack my="20px" direction="column" gap={4}>
-
+          {propertyReferralsInfo.map((bar) =>
+          <ProgressBar
+            key={bar.title} {...bar}/>)}
         </Stack>
     </Box>
   )
