b6c7305fffd7e147c563b1a856975b2c4b359481
Ryan <ryanz35@hotmail.com>
2023-03-03 18:00:29 -0500

    starting form, handling submit button

--- src/components/layout/common/Form.tsx
+++ src/components/layout/common/Form.tsx
@@ -12,6 +12,17 @@ const Form = ({ type, register, handleSubmit,
       <Typography fontSize={25} fontWeight={700} color="#11142d">
         {type} a Property
       </Typography>
+      <Box mt={2.5} borderRadius="5px" padding="20px" bgcolor="#D3D3D3">
+        <form style={{
+          marginTop:"20px",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          gap: "20px"}}
+          onSubmit={handleSubmit(onFinishHandler)}>
+            
+        </form>
+      </Box>
     </Box>
   )
 }
