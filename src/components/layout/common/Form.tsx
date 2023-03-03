8122b98679ca8e38a7e90f395328735421b88ef8
Ryan <ryanz35@hotmail.com>
2023-03-03 18:16:49 -0500

    shadow for consis

--- src/components/layout/common/Form.tsx
+++ src/components/layout/common/Form.tsx
@@ -1,5 +1,5 @@
 import { Box, Typography, FormControl, FormHelperText,
-  TextField, TextareaAutosize, Stack, Select, MenuItem, Button }
+  TextField, TextareaAutosize, Stack, Select, MenuItem, Button, color, fontWeight }
   from "@pankod/refine-mui"
 
 import { FormProps } from "interfaces/common";
@@ -12,15 +12,19 @@ const Form = ({ type, register, handleSubmit,
       <Typography fontSize={25} fontWeight={700} color="#11142d">
         {type} a Property
       </Typography>
-      <Box mt={2.5} borderRadius="5px" padding="20px" bgcolor="#D3D3D3">
+      <Box mt={2.5} borderRadius="5px" padding="20px" bgcolor="#D3D3D3" boxShadow= "2px 1px 2px #7393B3">
         <form style={{
           marginTop:"20px",
           width: "100%",
           display: "flex",
           flexDirection: "column",
-          gap: "20px"}}
+          gap: "20px",
+          }}
           onSubmit={handleSubmit(onFinishHandler)}>
-            
+            <FormControl>
+              <FormHelperText sx={{color: "#5A5A5A"}}>Enter Property Name</FormHelperText>
+              <TextField />
+            </FormControl>
         </form>
       </Box>
     </Box>
