a4cfa47c57a603acfb498555da4eacd2e0ae1145
Ryan <ryanz35@hotmail.com>
2023-03-02 19:10:49 -0500

    imports for create, setting up create page

--- src/pages/create-property.tsx
+++ src/pages/create-property.tsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import { useState } from 'react'
+// useGetIdentity is used to get information about 
+// the user like name, email and photo
+import { useGetIdentity } from '@pankod/refine-core'
+// help to check form for values
+import { FieldValues, useForm } from '@pankod/refine-react-hook-form'
+import { useNavigate } from '@pankod/refine-react-router-v6'
+
+import Form from 'components/layout/common/Form'
 
 const CreateProperty = () => {
+
+  const navigate = useNavigate();
+  //declaring data and renaming it to user.
+  // allows us to get info from the form
+  const { data: user } = useGetIdentity();
+  const [propertyImage, setPropertyImage] = useState({
+    name: '',
+    url: '',
+  })
+
+  const { refineCore: { onFinish, formLoading}, register, handleSubmit } = useForm();
+
   return (
     <div>create-proptery</div>
   )
